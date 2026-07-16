6937bf6ba28b10be8bbe7135f279b041b95b51ba
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2026-07-15 21:45:43 -0300

    - GET /movies deve retornar 200 quando não forem informados argumentos

--- dsmovie-restassured/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ dsmovie-restassured/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -3,33 +3,46 @@ package com.devsuperior.dsmovie.controllers;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+
 public class MovieControllerRA {
-	
-	@Test
-	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
-	}
-	
-	@Test
-	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {		
-	}
-	
-	@Test
-	public void findByIdShouldReturnMovieWhenIdExists() {		
-	}
-	
-	@Test
-	public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() {	
-	}
-	
-	@Test
-	public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndBlankTitle() throws JSONException {		
-	}
-	
-	@Test
-	public void insertShouldReturnForbiddenWhenClientLogged() throws Exception {
-	}
-	
-	@Test
-	public void insertShouldReturnUnauthorizedWhenInvalidToken() throws Exception {
-	}
+
+    void setUp() {
+        baseURI = "http://localhost:8080";
+    }
+
+
+    @Test
+    public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
+        given()
+            .when()
+                .get("/movies")
+            .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {
+    }
+
+    @Test
+    public void findByIdShouldReturnMovieWhenIdExists() {
+    }
+
+    @Test
+    public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() {
+    }
+
+    @Test
+    public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndBlankTitle() throws JSONException {
+    }
+
+    @Test
+    public void insertShouldReturnForbiddenWhenClientLogged() throws Exception {
+    }
+
+    @Test
+    public void insertShouldReturnUnauthorizedWhenInvalidToken() throws Exception {
+    }
 }
