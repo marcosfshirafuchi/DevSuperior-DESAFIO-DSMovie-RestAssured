a2e358c655fe2c089fc20dc98e9dd1230f73d632
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2026-07-16 16:22:08 -0300

    - POST /movies deve retornar 422 quando informado nome em branco

--- dsmovie-restassured/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ dsmovie-restassured/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -1,8 +1,14 @@
 package com.devsuperior.dsmovie.controllers;
 
+import com.devsuperior.dsmovie.tests.TokenUtil;
+import io.restassured.http.ContentType;
 import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
@@ -11,15 +17,31 @@ import static org.hamcrest.Matchers.is;
 
 public class MovieControllerRA {
 
-    private String existingMovieTitle;
+    private String existingMovieTitle, blankMovieTitle;
     private Long existingMovieId, nonExistingMovieId;
+    private String adminUsername, adminPassword;
+    private String adminToken;
+
+    private Map<String, Object> postMovieInstance;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws JSONException {
         baseURI = "http://localhost:8080";
         existingMovieTitle = "Matrix";
         existingMovieId = 7L;
         nonExistingMovieId = 100L;
+        blankMovieTitle = "";
+
+        adminUsername = "maria@gmail.com";
+        adminPassword = "123456";
+
+        adminToken = TokenUtil.obtainAccessToken(adminUsername, adminPassword);
+
+        postMovieInstance = new HashMap<>();
+        postMovieInstance.put("title","Test Movie");
+        postMovieInstance.put("score",0.0);
+        postMovieInstance.put("count",0);
+        postMovieInstance.put("image","https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg");
     }
 
 
@@ -71,6 +93,18 @@ public class MovieControllerRA {
 
     @Test
     public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndBlankTitle() throws JSONException {
+        postMovieInstance.put("title",blankMovieTitle);
+        JSONObject newMovie = new JSONObject(postMovieInstance);
+        given()
+            .header("Content-type","application/json")
+            .header("Authorization","Bearer " + adminToken)
+            .body(newMovie.toString())
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+        .when()
+            .post("/movies")
+        .then()
+            .statusCode(422);
     }
 
     @Test
